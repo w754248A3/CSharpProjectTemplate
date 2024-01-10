e9872683acdaea59b4ef40499284e581775b5a66
猪无能 <798158053@qq.com>
2024-01-10 19:10:00 +0800

    初始化

---
+++ MyConTemp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+
+
+
+namespace MyConTemp
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好,世界");
+        }
+    }
+}
